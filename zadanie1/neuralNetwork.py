f791c8eb862482c7a3e0fd810080600171ed189c
nicolas-droppa <droppa.nicolas@gmail.com>
2025-10-15 22:08:11 +0200

    added dropout

--- zadanie1/neuralNetwork.py
+++ zadanie1/neuralNetwork.py
@@ -7,8 +7,10 @@ class MLP(nn.Module):
         self.net = nn.Sequential(
             nn.Linear(input_dim, 512),
             nn.ReLU(),
+            nn.Dropout(p=0.2),
             nn.Linear(512, 128),
             nn.ReLU(),
+            nn.Dropout(p=0.1),
             nn.Linear(128, 32),
             nn.ReLU(),
             nn.Linear(32, 2),
